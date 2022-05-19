79ef5297ddbe26686126338cadfe8a98535713bd
Yano Omurai <52166982+thanhtaimrjoe@users.noreply.github.com>
2022-05-19 08:26:40 +0700

    Fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:yama_shopping/modal/cart.dart';
 import 'package:yama_shopping/modal/user.dart';
@@ -8,7 +7,6 @@ import 'package:yama_shopping/screen/catalog.dart';
 import 'package:yama_shopping/screen/product.dart';
 import 'package:yama_shopping/services/category_service.dart';
 import 'screen/login.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +14,6 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   CategoryService categoryService = CategoryService();
   // This widget is the root of your application.
   @override
@@ -35,29 +32,14 @@ class MyApp extends StatelessWidget {
             create: (context) => Cart(),
           ),
         ],
-        child: FutureBuilder(
-          future: _fbApp,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              print('You have an error! ${snapshot.error.toString()}');
-              // ignore: prefer_const_constructors
-              return Center(child: Text('Something went wrong'));
-            } else if (snapshot.hasData) {
-              return MaterialApp(
-                theme: ThemeData(backgroundColor: Colors.teal[400]),
-                initialRoute: '/',
-                routes: {
-                  '/': (context) => MyLogin(),
-                  '/catalog': (context) => MyCatalog(),
-                  '/product': (context) => MyProduct(),
-                  '/cart': (context) => MyCart(),
-                },
-              );
-            } else {
-              return SpinKitCubeGrid(
-                color: Colors.teal[400],
-              );
-            }
+        child: MaterialApp(
+          theme: ThemeData(backgroundColor: Colors.teal[400]),
+          initialRoute: '/',
+          routes: {
+            '/': (context) => MyLogin(),
+            '/catalog': (context) => MyCatalog(),
+            '/product': (context) => MyProduct(),
+            '/cart': (context) => MyCart(),
           },
         ));
   }
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,99 +1,121 @@
 import 'dart:math';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:yama_shopping/modal/user.dart';
 
 class MyLogin extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final Color theme = Theme.of(context).backgroundColor;
     User user = Provider.of<User>(context);
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/shopping-bag.png',
-              width: 150,
-              height: 150,
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'YAMA SHOPPING',
-              style: TextStyle(
-                  fontSize: 24, fontWeight: FontWeight.bold, color: theme),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Username'),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Password'),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                var result = true;
-                if (usernameController.text != user.username) {
-                  result = false;
-                }
-                if (passwordController.text != user.password) {
-                  result = false;
-                }
-                if (result) {
-                  //test firebase database
-                  DatabaseReference _testRef =
-                      FirebaseDatabase.instance.ref().child("test");
-                  _testRef
-                      .set("Hello firebase database ${Random().nextInt(100)}");
+    return FutureBuilder(
+      future: _fbApp,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print('You have an error! ${snapshot.error.toString()}');
+          // ignore: prefer_const_constructors
+          return Scaffold(body: Center(child: Text('Something went wrong')));
+        } else if (snapshot.hasData) {
+          return Scaffold(
+            body: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/shopping-bag.png',
+                    width: 150,
+                    height: 150,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'YAMA SHOPPING',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: theme),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    controller: usernameController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Username'),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Password'),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      var result = true;
+                      if (usernameController.text != user.username) {
+                        result = false;
+                      }
+                      if (passwordController.text != user.password) {
+                        result = false;
+                      }
+                      if (result) {
+                        //test firebase database
+                        DatabaseReference _testRef =
+                            FirebaseDatabase.instance.ref().child("test");
+                        _testRef.set(
+                            "Hello firebase database ${Random().nextInt(100)}");
 
-                  Navigator.pushReplacementNamed(context, '/catalog');
-                } else {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) => AlertDialog(
-                            title: const Text('Warning'),
-                            content: const Text(
-                                'Your username or password was wrong. Please try again'),
-                            actions: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pop(context, 'OK');
-                                },
-                                child: Text('OK'),
-                                style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all(theme)),
-                              )
-                            ],
-                          ));
-                }
-              },
-              child: Text('Login'),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(theme)),
-            )
-          ],
-        ),
-      ),
+                        Navigator.pushReplacementNamed(context, '/catalog');
+                      } else {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                                  title: const Text('Warning'),
+                                  content: const Text(
+                                      'Your username or password was wrong. Please try again'),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context, 'OK');
+                                      },
+                                      child: Text('OK'),
+                                      style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all(theme)),
+                                    )
+                                  ],
+                                ));
+                      }
+                    },
+                    child: Text('Login'),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(theme)),
+                  )
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Scaffold(
+            body: SpinKitCubeGrid(
+              color: Colors.teal[400],
+            ),
+          );
+        }
+      },
     );
   }
 }
