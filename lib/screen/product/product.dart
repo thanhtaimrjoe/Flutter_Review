2e0e47caf74e3b92f77b2ba3f31ac06af308d8a4
Yano Omurai <52166982+thanhtaimrjoe@users.noreply.github.com>
2022-05-24 13:18:48 +0700

    Design sliver appbar

--- lib/screen/product/product.dart
+++ lib/screen/product/product.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:yama_shopping/modal/product.dart';
 
@@ -11,51 +13,80 @@ class MyProductPage extends StatelessWidget {
     Product product = Product(
         argument['id'], argument['name'], argument['price'], argument['image']);
     return Scaffold(
-        body: NestedScrollView(
-      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        body: CustomScrollView(
+      slivers: [
         SliverAppBar(
+          title: Text(product.name),
           floating: true,
-          snap: true,
-          title: Text('AppBar'),
-          centerTitle: true,
-        )
-      ],
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(8),
-              width: size.width,
-              height: 200,
-              color: Colors.amberAccent,
-            ),
-            Container(
-              margin: const EdgeInsets.all(8),
-              width: size.width,
-              height: 200,
-              color: Colors.black26,
-            ),
-            Container(
-              margin: const EdgeInsets.all(8),
-              width: size.width,
-              height: 200,
-              color: Colors.greenAccent,
-            ),
-            Container(
-              margin: const EdgeInsets.all(8),
-              width: size.width,
-              height: 200,
-              color: Colors.redAccent,
-            ),
-            Container(
-              margin: const EdgeInsets.all(8),
-              width: size.width,
-              height: 200,
-              color: Colors.pinkAccent,
-            ),
+          pinned: true,
+          expandedHeight: 200,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios)),
+          actions: [
+            Icon(Icons.search),
           ],
+          flexibleSpace: FlexibleSpaceBar(
+            background: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(product.image), fit: BoxFit.fitWidth),
+                //color: const Color.fromARGB(125, 2, 16, 53),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Hello"),
+                    Text("Alibaba"),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
-      ),
+        SliverToBoxAdapter(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  width: size.width,
+                  height: 200,
+                  color: Colors.amberAccent,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  width: size.width,
+                  height: 200,
+                  color: Colors.black26,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  width: size.width,
+                  height: 200,
+                  color: Colors.greenAccent,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  width: size.width,
+                  height: 200,
+                  color: Colors.redAccent,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  width: size.width,
+                  height: 200,
+                  color: Colors.pinkAccent,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     ));
   }
 }
